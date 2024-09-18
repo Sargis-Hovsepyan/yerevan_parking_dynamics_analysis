a04507001c3f4e85607c4c18b4604996072ee31d
Sargis Hovsepyan <60169060+Sargis-Hovsepyan@users.noreply.github.com>
2024-09-18 13:13:49 +0400

    Deleted

--- lib/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-Module:         POSITRON
-
-Date:           29/02/20204
-Author:         Sargis Hovsepyan
-    
-Desrcription:   The following module is created to deal with GPS realatid data,
-                starting from pre-processing to separate graph algorithms.
-'''
